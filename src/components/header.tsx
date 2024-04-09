c7170e623f97eae7171c3d30950ef7dd0fe1067c
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-08 23:46:23 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+        <img src="react.svg" alt="React Quiz logo" />
+
+        <h1>The React Quiz</h1>
+    </header>
+  )
+}
